716009ae76d4f2cc651e85df3ff552f5d150c1ec
Dima Kopfmann <dmitry.golovinov@jetbrains.com>
2026-03-06 13:31:25 +0100

    build: align project JDK config to Java 21

--- build-logic/src/main/kotlin/kotlin-common.gradle.kts
+++ build-logic/src/main/kotlin/kotlin-common.gradle.kts
@@ -1,10 +1,19 @@
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+
 plugins {
     kotlin("jvm")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 kotlin {
     jvmToolchain(21)
     compilerOptions {
+        // Keep Kotlin language/api version on plugin defaults.
         allWarningsAsErrors.set(true)
         freeCompilerArgs.addAll(
             "-Xjsr305=strict",
