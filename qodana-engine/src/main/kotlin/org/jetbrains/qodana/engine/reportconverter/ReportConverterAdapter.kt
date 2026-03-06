626a293495700598b871cc4eae1914ad2335a4a6
Dima Kopfmann <dmitry.golovinov@jetbrains.com>
2026-03-06 13:31:35 +0100

    Fix report converter output dir handling

--- qodana-engine/src/main/kotlin/org/jetbrains/qodana/engine/reportconverter/ReportConverterAdapter.kt
+++ qodana-engine/src/main/kotlin/org/jetbrains/qodana/engine/reportconverter/ReportConverterAdapter.kt
@@ -2,16 +2,26 @@ package org.jetbrains.qodana.engine.reportconverter
 
 import org.jetbrains.qodana.engine.port.ReportConverter
 import org.jetbrains.qodana.sarif.QodanaReportConverter
+import java.nio.file.Files
 import java.nio.file.Path
 
 class ReportConverterAdapter : ReportConverter {
 
     override fun convertToHtml(resultsDir: Path, outputDir: Path) {
+        prepareOutputDir(outputDir)
         val options = QodanaReportConverter.Options(
             Integer.MAX_VALUE,
             resultsDir.toFile(),
             outputDir.toFile(),
         )
         QodanaReportConverter(options).convert()
+    }
+
+    internal fun prepareOutputDir(outputDir: Path) {
+        val outputFile = outputDir.toFile()
+        if (outputFile.exists() && !outputFile.deleteRecursively()) {
+            error("Failed to clear report output directory: $outputDir")
+        }
+        Files.createDirectories(outputDir)
     }
 }
