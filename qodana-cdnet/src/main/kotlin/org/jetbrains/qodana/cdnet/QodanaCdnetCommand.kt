556e044384588fcc5789f10ae23d492ce97be199
Anna Zhukova <anna.zhukova@jetbrains.com>
2026-05-26 19:06:10 +0200

    feat: add --version/-v option to qodana-clang and qodana-cdnet root commands

--- qodana-cdnet/src/main/kotlin/org/jetbrains/qodana/cdnet/QodanaCdnetCommand.kt
+++ qodana-cdnet/src/main/kotlin/org/jetbrains/qodana/cdnet/QodanaCdnetCommand.kt
@@ -2,11 +2,16 @@ package org.jetbrains.qodana.cdnet
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.Context
+import com.github.ajalt.clikt.parameters.options.versionOption
 
 class QodanaCdnetCommand : CliktCommand("qodana-cdnet") {
     override fun help(context: Context) = "Qodana for .NET (ReSharper InspectCode) v${Version.VALUE}"
 
     override val invokeWithoutSubcommand = true
+
+    init {
+        versionOption(Version.VALUE, names = setOf("--version", "-v"))
+    }
 
     override fun run() {
         if (currentContext.invokedSubcommand == null) {
