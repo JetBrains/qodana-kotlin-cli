5748c9022be370b75f1cddda2fa545ac7982434f
Anna Zhukova <anna.zhukova@jetbrains.com>
2026-05-27 13:08:48 +0200

    revert: --version on clang/cdnet + native-smoke job (QD-14728, QD-14814 follow-up)

--- qodana-clang/src/main/kotlin/org/jetbrains/qodana/clang/QodanaClangCommand.kt
+++ qodana-clang/src/main/kotlin/org/jetbrains/qodana/clang/QodanaClangCommand.kt
@@ -2,16 +2,11 @@ package org.jetbrains.qodana.clang
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.Context
-import com.github.ajalt.clikt.parameters.options.versionOption
 
 class QodanaClangCommand : CliktCommand("qodana-clang") {
     override fun help(context: Context) = "Qodana for C/C++ (clang-tidy) v${Version.VALUE}"
 
     override val invokeWithoutSubcommand = true
-
-    init {
-        versionOption(Version.VALUE, names = setOf("--version", "-v"))
-    }
 
     override fun run() {
         if (currentContext.invokedSubcommand == null) {
