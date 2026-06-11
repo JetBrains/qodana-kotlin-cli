89d19f72a07541e3f10c3a72fb6f15b20492b427
Anna Zhukova <anna.zhukova@jetbrains.com>
2026-06-11 11:23:53 +0200

    feat(linter-images): register provision-dist in image-tool root command

--- linter-images/src/main/kotlin/org/jetbrains/qodana/images/Main.kt
+++ linter-images/src/main/kotlin/org/jetbrains/qodana/images/Main.kt
@@ -1,7 +1,28 @@
 package org.jetbrains.qodana.images
 
+import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.core.subcommands
+import org.jetbrains.qodana.images.dist.DistVerifier
+import org.jetbrains.qodana.images.dist.FeedClient
+import org.jetbrains.qodana.images.dist.TarGzExtractor
+import org.jetbrains.qodana.images.process.ProcessCommandRunner
 
-fun main(args: Array<String>) {
-    ImageToolCommand().main(args)
+/**
+ * Assembles the `image-tool` root command with production collaborators (canonical factory).
+ * One real `ProcessCommandRunner` backs FeedClient + DistVerifier (curl/gpg/sha256sum through the port);
+ * there is no separate downloader. Later phases add install-cli + verify-dist-layout here.
+ */
+fun buildImageTool(): CliktCommand {
+    val runner = ProcessCommandRunner()
+    return ImageToolCommand().subcommands(
+        ProvisionDistCommand(
+            feedClient = FeedClient(runner),
+            verifier = DistVerifier(runner),
+            extractor = TarGzExtractor(),
+            getEnv = System::getenv,
+        ),
+    )
 }
+
+fun main(args: Array<String>) = buildImageTool().main(args)
